f7504367880b3bfd83fcd571ae6cf821e24d2111
Jose Fernando Chavez Castillo <josefernandochca@gmail.com>
2022-10-28 12:26:13 -0500

    Actualizacion de javascript

--- js1.js
+++ js1.js
@@ -1,11 +1,10 @@
-console.log("Hola Java Script");
-console.warn("Hola");
-console.error("AAAAAAAAAAAAAAAAAAAAA");
-console.count("2");
+console.log("Hello my friend he has chesse");
+console.warn("Esto es un aviso");
+console.error("Ah ocurrido un error");
+console.count("3");
 
 console.log(typeof 4);
 console.log("Tipo de 34: " + typeof 34+".");
-/* se agrega ${} para poner la funcion dentro del texto */
 console.log(`Tipo de 34: ${typeof 34}.`);
 console.log();
 console.log('hola');
@@ -20,14 +19,11 @@ console.log(`4 es igual a 3 y 4 es mayor a 2 ${4 == 3 && 4 > 2}`);
 console.log(`4 es igual a 3 pero su resultado es el opuesto, y 4 es mayor a 2 ${!(4 == 3) && 4 > 2}`);
 console.log()
 
-console.log(10 < 2 ? "OMG ES VERDAD NO WAY":"Es falso pipipi")
+console.log(10 < 2 ? "Es verdad":"Es falso")
 
 let i;
 let nombre;
-let edad =33, ciudad="Monterrey";
-
-/* CONSTANTES DEBES DE SER MAYOR */
-/* Las constantes no se les puede cambiar el valor */
+let edad =19, ciudad="Monterrey";
 
 const FACTOR = 1.45;
 const VEL = 1500;
@@ -40,11 +36,3 @@ let mensaje;
 let temp =4;
 mensaje = temp > 10? "Temperatura ambiente":"Temperatura fria";
 console.log(mensaje);
-
-const SEMESTRES =9;
-
-/* SI SE INTENTA MODIDIFICAR SE ROMPE EL PROGRAMA 
-SEMESTRES = 3; */
-
-
-
